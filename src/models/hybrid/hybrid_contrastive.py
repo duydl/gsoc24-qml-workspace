12a0d6a1be6588e83b39c7138802f849dc7049e7
duydl <duy.dole.00ece@gmail.com>
2024-06-20 22:16:47 +0700

    [hybrid] Update hybrid model and runner script

--- src/models/hybrid/hybrid_contrastive.py
+++ src/models/hybrid/hybrid_contrastive.py
@@ -1,79 +1,53 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_lightning as pl
+
 import torchmetrics
+import pennylane as qml
+import matplotlib.pyplot as plt
+
 from pytorch_metric_learning import losses
 
 import sys
 sys.path.append("../../")
 from utils.utils import get_preprocessing
+from models.classical.models import MNISTConvEncoder
 
-_ACTIVATIONS = {"relu": nn.ReLU, "gelu": nn.GELU}
-_POOLING = {"max": nn.MaxPool2d, "avg": nn.AvgPool2d}
 
-class ConvUnit(nn.Module):
-    """
-    A convolutional unit consisting of a convolutional layer, batch normalization, activation, and pooling.
-    """
-    def __init__(self, input_channels, output_channels, kernel_size, stride, padding, pool_type, pool_kernel_size, pool_stride, activ_type):
-        super().__init__()
-        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size, stride, padding)
-        self.bn = nn.BatchNorm2d(output_channels)
-        self.activ = _ACTIVATIONS[activ_type]()
-        self.pool = _POOLING[pool_type](kernel_size=pool_kernel_size, stride=pool_stride)
-
-    def forward(self, x):
-        return self.pool(self.activ(self.bn(self.conv(x))))
-
-class MNISTConvEncoder(nn.Module):
-    """
-    A convolutional encoder for MNIST images.
-    """
-    backbone_output_size = 196
-    output_size = 8
-    def __init__(self, activ_type, pool_type):
-        super().__init__()
-        self.conv_unit1 = ConvUnit(1, 4, 3, 1, 1, pool_type, 2, 2, activ_type)
-        self.conv_unit2 = ConvUnit(4, 4, 3, 1, 1, pool_type, 2, 2, activ_type)
-        self.fc1 = nn.Linear(self.backbone_output_size, self.output_size)
-
-    def forward(self, x):
-        x = self.conv_unit1(x)
-        x = self.conv_unit2(x)
-        x = x.view(-1, self.backbone_output_size)
-        return self.fc1(x)
-
-import pennylane as qml
 class QuantumHead(nn.Module):
-    def __init__(self, in_features, out_features, n_qlayers):
+    def __init__(self, in_features, n_qubits, out_features, n_qlayers):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.n_qubits = [in_features,out_features][in_features<out_features]
+        self.n_qubits = n_qubits
         self.n_qlayers = n_qlayers
+        
+        self.proj = nn.Linear(in_features, n_qubits)
         
         self.device = qml.device('default.qubit', wires=self.n_qubits)
         @qml.qnode(self.device, interface='torch')
         def quantum_circuit(inputs, weights):
             # print(inputs.shape)
-            qml.templates.AngleEmbedding(inputs, wires=range(self.in_features))
+            qml.templates.AngleEmbedding(inputs, wires=range(self.n_qubits))
             
             # Apply layers of rotation gates and CNOTs for entanglement
-            for layer in range(n_qlayers):
+            for layer in range(self.n_qlayers):
                 for qubit in range(self.n_qubits):
                     qml.RX(weights[layer, qubit, 0], wires=qubit)
                     qml.RY(weights[layer, qubit, 1], wires=qubit)
                     qml.RZ(weights[layer, qubit, 2], wires=qubit)
                 for qubit in range(self.n_qubits - 1):
                     qml.CNOT(wires=[qubit, qubit + 1])
-                
+                qml.CNOT(wires=[self.n_qubits - 1, 0])
             return [qml.expval(qml.PauliZ(i)) for i in range(self.out_features)]
         
         self.quantum_circuit = quantum_circuit
-        dummy_inputs = torch.randn(self.in_features)
+        dummy_inputs = torch.randn(self.n_qubits)
         dummy_weights = torch.randn(self.n_qlayers, self.n_qubits, 3)
         print(qml.draw(self.quantum_circuit)(dummy_inputs, dummy_weights))
+        
+        qml.draw_mpl(self.quantum_circuit)(dummy_inputs, dummy_weights)
+        plt.show()
         
         self.quantum_layer = qml.qnn.TorchLayer(self.quantum_circuit, {"weights": (n_qlayers, self.n_qubits, 3)})
         
@@ -82,19 +56,20 @@ class QuantumHead(nn.Module):
         # print(self.quantum_layer(x).shape)
         
     def forward(self, inputs):
-        q_outputs = self.quantum_layer(inputs)
+        q_inputs = self.proj(inputs)
+        q_outputs = self.quantum_layer(q_inputs)
         return q_outputs
 
-class MNISTQSupContrast(pl.LightningModule):
+class MNISTHybridSupContrast(pl.LightningModule):
     """
     A PyTorch Lightning module for supervised contrastive learning on the MNIST dataset.
     """
-    def __init__(self, activ_type, pool_type, head_output, n_qlayers, lr, pos_margin=0.25, neg_margin=1.5, preprocess=None):
+    def __init__(self, activ_type, pool_type, head_output, n_qubits, n_qlayers, lr, pos_margin=0.25, neg_margin=1.5, preprocess=None):
         super().__init__()
         self.save_hyperparameters()
         self.preprocessing = get_preprocessing(preprocess)
         self.encoder = MNISTConvEncoder(activ_type, pool_type)
-        self.head = QuantumHead(MNISTConvEncoder.output_size, head_output, n_qlayers)
+        self.head = QuantumHead(MNISTConvEncoder.backbone_output_size, n_qubits, head_output, n_qlayers)
         self.loss = losses.ContrastiveLoss(pos_margin=pos_margin, neg_margin=neg_margin)
         self.train_loss = torchmetrics.MeanMetric()
         self.valid_loss = torchmetrics.MeanMetric()
--- src/runner_hybrid.py
+++ src/runner_hybrid.py
@@ -3,52 +3,44 @@ import torch
 import numpy as np
 import pandas as pd
 from torchvision import datasets, transforms as T
-from torch.utils.data import DataLoader, random_split, Subset
+from torch.utils.data import DataLoader, random_split, Subset, TensorDataset
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning import Trainer
-from models.hybrid.hybrid_contrastive import MNISTQSupContrast
-from utils.utils import generate_embeddings, pca_proj, tsne_proj
+from models.hybrid.hybrid_contrastive import MNISTHybridSupContrast
+from utils.utils import generate_embeddings, vmf_kde_on_circle, pca_proj, tsne_proj
+from utils.data_mnist import load_mnist_data
 
 import matplotlib.pyplot as plt
-
-def load_data():
-    """
-    Load and preprocess the MNIST dataset.
-
-    Returns:
-        tuple: Training and validation data loaders.
-    """
-    transform = T.Compose([T.ToTensor(), T.Normalize((0.1307,), (0.3081,))])
-    dataset = datasets.MNIST(root="../data/", train=True, download=True, transform=transform)
-
-    filtered_indices = [i for i, (_, label) in enumerate(dataset) if label in [0, 1, 5]]
-    filtered_dataset = Subset(dataset, filtered_indices)
-
-    train_size = int(0.9 * len(filtered_dataset))
-    val_size = len(filtered_dataset) - train_size
-    train_dataset, val_dataset = random_split(filtered_dataset, [train_size, val_size])
-
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=12)
-    val_loader = DataLoader(val_dataset, batch_size=64, num_workers=12)
-
-    return train_loader, val_loader
 
 # Initialize model
 def main():
     """
     Main function to train the model and generate embeddings.
     """
-    train_loader, val_loader = load_data()
+    classes = (3, 6, 9)
+    reduced_dim = 10
+    dataset_size = (1000, 300)
 
-    model = MNISTQSupContrast(activ_type="relu", pool_type="max", head_output=2, lr=1e-3, n_qlayers=1)
+    mnist_data = load_mnist_data(classes=classes, reduced_dim = reduced_dim, dataset_size=dataset_size, data_dir="../data/")
+    
+    def create_data_loader(data, labels, batch_size=64, shuffle=True, num_workers=4):
+        dataset = TensorDataset(data, labels)
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+        return data_loader
+    
+    train_loader = create_data_loader(mnist_data["train_data"], mnist_data["train_labels"])
+    val_loader = create_data_loader(mnist_data["test_data"], mnist_data["test_labels"])
+
+    model = MNISTHybridSupContrast(activ_type="relu", pool_type="max", n_qubits=8, head_output=2, lr=1e-3, n_qlayers=3)
 
     # Plot embeddings before training
     embeddings, labels = generate_embeddings(model, val_loader)
     pca_proj(embeddings, labels)
     tsne_proj(embeddings, labels)
+    vmf_kde_on_circle(embeddings, labels)
 
     # Training the model
-    logger = CSVLogger(save_dir="logs/", name="MNISTContrast", version=0)
+    logger = CSVLogger(save_dir="logs/", name="MNISTQContrast", version=0)
     trainer = Trainer(max_epochs=10, logger=logger, gpus=0)
     trainer.fit(model, train_loader, val_loader)
 
@@ -56,6 +48,7 @@ def main():
     embeddings, labels = generate_embeddings(model, val_loader)
     pca_proj(embeddings, labels)
     tsne_proj(embeddings, labels)
+    vmf_kde_on_circle(embeddings, labels)
 
     # Plot training and validation loss
     metrics_df = pd.read_csv(f"{logger.log_dir}/metrics.csv")
@@ -74,46 +67,6 @@ def main():
     plt.legend()
     plt.grid(True)
     plt.show()
-
-    # # Quantum encoding and SWAP test
-    # img1, label1 = val_loader.dataset[0]
-    # img2, label2 = val_loader.dataset[1]
-    # img1, img2 = img1.unsqueeze(0), img2.unsqueeze(0)
-
-    # model.eval()
-    # with torch.no_grad():
-    #     embedding1 = model.forward(img1).flatten().numpy()
-    #     embedding2 = model.forward(img2).flatten().numpy()
-
-    # fidelity = swap_test_circuit(embedding1, embedding2)
-    # print("Fidelity (result of SWAP test):", fidelity)
-
-    # Fidelity scores for a batch of samples
-    # reference_image, reference_label = val_loader.dataset[0]
-    # reference_embedding = model.forward(reference_image.unsqueeze(0)).detach().numpy().flatten()
-
-    # embeddings = []
-    # labels = []
-
-    # for i in range(100):
-    #     image, label = val_loader.dataset[i]
-    #     embedding = model.forward(image.unsqueeze(0)).detach().numpy().flatten()
-    #     embeddings.append(embedding)
-    #     labels.append(label)
-
-    # fidelity_scores = []
-    # for embedding, label in zip(embeddings, labels):
-    #     fidelity = swap_test_circuit(reference_embedding, embedding)
-    #     fidelity_scores.append((fidelity.item(), str(label)))
-
-    # df = pd.DataFrame(fidelity_scores, columns=['Fidelity', 'Label'])
-
-    # plt.figure(figsize=(10, 6))
-    # sns.kdeplot(data=df, x='Fidelity', hue='Label', common_norm=False, fill=True)
-    # plt.title('Fidelity Score Distributions by Label')
-    # plt.xlabel('Fidelity Score')
-    # plt.ylabel('Density')
-    # plt.show()
 
 if __name__ == "__main__":
     main()
